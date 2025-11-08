58f8798489267a1eb05c4a375959b9b526c2c30d
Sssss-Mmm <elation4927@gmail.com>
2025-11-08 20:47:11 +0900

    langgraph_02

--- Ch06.LangGraph/ch06_LANG_GRAPH_04.py
+++ Ch06.LangGraph/ch06_LANG_GRAPH_04.py
@@ -50,19 +50,11 @@ graph_builder.add_edge("tools","chatbot")
 graph_builder.add_edge(START,"chatbot")
 
 # 체크포인터를 지정하여 그래프를 컴파일
-graph = graph_builder.compile(checkpointer=memory)
+graph = graph_builder.compile(checkpointer=memory, interrupt_before=["tools"])
 
-config = {"configurable":{"thread_id":"1"}}
+config = {"configurable":{"thread_id":"2"}}
 
-user_input = "안녕! 내 이름은 오해원이야."
-
-events = graph.stream(
-    {"messages": [("user", user_input)]}, config, stream_mode="values"
-)
-for event in events:
-    event["messages"][-1].pretty_print()
-
-user_input = "내 이름 기억하니"
+user_input = "지금 서울 날씨 어때?"
 
 events = graph.stream(
     {"messages": [("user", user_input)]}, config, stream_mode="values"
@@ -72,3 +64,56 @@ for event in events:
 
 snapshot = graph.get_state(config)
 print(snapshot)
+
+from langchain_core.messages import AIMessage
+from langchain_core.messages import ToolMessage
+
+
+# 최근 메세지
+existing_message = snapshot.values["messages"][-1]
+# 최근 메세지의 id
+existing_message_id = existing_message.tool_calls[0]["id"]
+
+# 강제할 응답 정의
+answer = ("서울의 날씨는 매우 맑아요.")
+
+# 강제할 응답을 포함한 메세지 상태 정의
+new_messages = [
+    ToolMessage(content=answer, tool_call_id = existing_message_id),
+    AIMessage(content=answer)
+]
+
+graph.update_state(
+    config,
+    {"messages": new_messages}
+)
+
+print("\n\nLast 2 messages:")
+print(graph.get_state(config).values["messages"][-2:])
+
+
+from langchain_core.messages import AIMessage
+
+user_input = "지금 서울 날씨 어때?"
+config = {"configurable": {"thread_id": "3"}}
+events = graph.stream(
+    {"messages": [("user", user_input)]}, config, stream_mode="values"
+)
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
+
+snapshot = graph.get_state(config)
+existing_message = snapshot.values["messages"][-1]
+new_tool_call = existing_message.tool_calls[0].copy()
+new_tool_call["args"]["query"] = "지금 경기도 날씨 어때?"
+new_message = AIMessage(
+    content=existing_message.content,
+    tool_calls=[new_tool_call],
+    id=existing_message.id,
+)
+
+graph.update_state(config, {"messages": [new_message]})
+
+print("\n\nLast 2 messages;")
+print(graph.get_state(config).values["messages"][-2:])
